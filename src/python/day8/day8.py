1c50b8555af985b7ac49aeb0ee33c07b30db10d1
Daniel Albarral Nunez <daniel@albarralnunez.me>
2022-12-08 20:36:10 -0800

    Day 8 Solution Minor Fix 🔧🍬

--- src/python/day8/day8.py
+++ src/python/day8/day8.py
@@ -1,82 +1,139 @@
 from typing import IO
 from pathlib import Path
 
+
 def cast_to_ints(row: str) -> list:
     return list(map(int, row))
 
+
 def read_file(f: IO) -> list:
-    return list(map(
-        cast_to_ints,
-        map(lambda x: x.strip(), f.readlines()
-    )))
+    return list(map(cast_to_ints, map(lambda x: x.strip(), f.readlines())))
 
-def is_visible_edge_x(
-    matrix: list[list[int]],
-    x: int, y: int
-) -> bool:
-    i = 0
-    value = matrix[x][y]
-    while x-i >= 0:
-        if matrix[x-i][y] >= value:
-            value = matrix[x-i][y]
-        else:
-            return False
-        i += 1
-    i = 0
-    value = matrix[x][y]
-    while x+i <= len(matrix) - 1:
-        if matrix[x+i][y] >= value:
-            value = matrix[x+i][y]
-        else:
+
+def is_in_the_edge(matrix: list[list[int]], x: int, y: int) -> bool:
+    return x == 0 or y == 0 or x == len(matrix) - 1 or y == len(matrix[0]) - 1
+
+
+def is_visible_edge_x_front(matrix: list[list[int]], x: int, y: int) -> bool:
+    i = 1
+    while x - i >= 0:
+        if matrix[x][y] <= matrix[x - i][y]:
             return False
         i += 1
     return True
 
-def is_visible_edge_y(
-    matrix: list[list[int]],
-    x: int, y: int
-) -> bool:
-    i = 0
-    value = matrix[x][y]
-    while y-i > 0:
-        if matrix[x][y-i] >= value:
-            value = matrix[x][y-i]
-        else:
-            return False
-        i += 1
-    i = 0
-    value = matrix[x][y]
-    while y+i < len(matrix[0]) - 1:
-        if matrix[x][y+i] >= value:
-            value = matrix[x][y+i]
-        else:
+
+def is_visible_edge_x_back(matrix: list[list[int]], x: int, y: int) -> bool:
+    i = 1
+    while x + i < len(matrix):
+        if matrix[x][y] <= matrix[x + i][y]:
             return False
         i += 1
     return True
+
+
+def is_visible_edge_y_back(matrix: list[list[int]], x: int, y: int) -> bool:
+    i = 1
+    while y - i >= 0:
+        if matrix[x][y] <= matrix[x][y - i]:
+            return False
+        i += 1
+    return True
+
+
+def is_visible_edge_y_front(matrix: list[list[int]], x: int, y: int) -> bool:
+    i = 1
+    while y + i < len(matrix[0]):
+        if matrix[x][y] <= matrix[x][y + i]:
+            return False
+        i += 1
+    return True
+
+
+def get_score_x_back(matrix: list[list[int]], x: int, y: int) -> int:
+    i = 1
+    while x - i > 0:
+        if matrix[x][y] <= matrix[x - i][y]:
+            break
+        i += 1
+    return i
+
+
+def get_score_x_forward(matrix: list[list[int]], x: int, y: int) -> int:
+    i = 1
+    while x + i < len(matrix) - 1:
+        if matrix[x][y] <= matrix[x + i][y]:
+            break
+        i += 1
+    return i
+
+
+def get_score_y_back(matrix: list[list[int]], x: int, y: int) -> int:
+    i = 1
+    while y - i > 0:
+        if matrix[x][y] <= matrix[x][y - i]:
+            break
+        i += 1
+    return i
+
+
+def get_score_y_forward(matrix: list[list[int]], x: int, y: int) -> int:
+    i = 1
+    while y + i < len(matrix[0]) - 1:
+        if matrix[x][y] <= matrix[x][y + i]:
+            break
+        i += 1
+    return i
+
 
 def is_visible(matrix: list[list[int]], x: int, y: int) -> bool:
-    if (
-        is_visible_edge_x(matrix, x, y) 
-        and is_visible_edge_y(matrix, x, y)
-    ):
+    if is_in_the_edge(matrix, x, y):
         return True
-    return False
+    result = (
+        is_visible_edge_x_front(matrix, x, y)
+        or is_visible_edge_x_back(matrix, x, y)
+        or is_visible_edge_y_front(matrix, x, y)
+        or is_visible_edge_y_back(matrix, x, y)
+    )
+    return result
 
-def problem_1(input_path: Path):
+
+def problem_1(problem_matrix: list[list[int]]):
     visible = 0
-    with input_path.open() as f:
-        problem_matrix = read_file(f)
-    for y, col in enumerate(problem_matrix):
-        for x, _ in enumerate(col):
+    for x, raw in enumerate(problem_matrix):
+        for y, _ in enumerate(raw):
             if is_visible(problem_matrix, x, y):
                 visible += 1
     return visible
 
+
+def get_score(matrix: list[list[int]], x: int, y: int) -> int:
+    score_x_back = get_score_x_back(matrix, x, y)
+    score_x_forward = get_score_x_forward(matrix, x, y)
+    score_y_back = get_score_y_back(matrix, x, y)
+    score_y_forward = get_score_y_forward(matrix, x, y)
+    result = score_x_back * score_x_forward * score_y_back * score_y_forward
+    return result
+
+
+def problem_2(problem_matrix: list[list[int]]):
+    max_score = 0
+    for x, raw in enumerate(problem_matrix):
+        for y, _ in enumerate(raw):
+            if is_in_the_edge(problem_matrix, x, y):
+                continue
+            score = get_score(problem_matrix, x, y)
+            max_score = max(max_score, score)
+    return max_score
+
+
 def main():
-    input_path = Path("files/day8/test_input.txt")
-    solution = problem_1(input_path)
-    print("Problem 1:")
-    print(solution)
+    # input_path = Path("files/day8/test_input.txt")
+    input_path = Path("files/day8/input.txt")
+    with input_path.open() as f:
+        problem_matrix = read_file(f)
+    print("Problem 1: ", problem_1(problem_matrix))
+    print("Problem 2: ", problem_2(problem_matrix))
 
 
 if __name__ == "__main__":
--- src/python/day8/test.py
+++ src/python/day8/test.py
@@ -1,27 +1,29 @@
 import pytest
-from day8 import is_visible_edge_x
+from day8 import is_visible, problem_2
 
 
 @pytest.mark.parametrize(
     "x, y, expected",
     (
+        (0, 1, True),
+        (1, 1, True),
+        (1, 3, False),
+        (2, 1, True),
         (2, 2, False),
-        # (0, 0, True),
-        # (0, 4, True),
-        # (4, 0, True),
-        # (3, 1, True),
-        # (3, 1, True),
-    )
+        (1, 2, True),
+        (1, 4, True),
+        (3, 4, True),
+    ),
 )
-def test_is_visible_edge_x(x, y, expected):
-    matrix = [
-        [1, 2, 3, 4, 5],
-        [2, 3, 4, 5, 6],
-        [3, 4, 5, 6, 7],
-        [4, 5, 6, 7, 8],
-        [5, 6, 7, 8, 9],
-    ]
+def test_is_visible(x, y, expected):
+    matrix_raw = ["30373", "25912", "65332", "33549", "35390"]
+    matrix: list[list[int]] = [list(map(int, row)) for row in matrix_raw]
     assert (
-        is_visible_edge_x(matrix, x, y) == expected,
-        f"Failed for {x}, {y}"
-    )
+        is_visible(matrix, x, y) == expected
+    ), f"Failed for {x}, {y}, value {matrix[x][y]}"
+
+
+def test_problem_2():
+    matrix_raw = ["30373", "25912", "65332", "33549", "35390"]
+    matrix: list[list[int]] = [list(map(int, row)) for row in matrix_raw]
+    assert problem_2(matrix) == 12
